a8a5b5722bff7e06139dca0b59398d3264382bff
iamyewon <rla357987@gmail.com>
2024-06-07 15:55:55 +0900

    modal close event(reset)적용중

--- js/modal.js
+++ js/modal.js
@@ -3,3 +3,39 @@ const closeModal = (id) => {
     const modalInstance = bootstrap.Modal.getInstance(modalElement);
     modalInstance.hide();
 }
+
+const resetModal = () => {
+    addUsername.value = '';
+    addEmail.value = '';
+    addTelephone.value = '';
+    addRole.value = 'user';
+    addPassword.value = '';
+
+    hiddenAlertMessage(addEmailMessage);
+    hiddenAlertMessage(addPasswordMessage);
+    hiddenAlertMessage(addTelephoneMessage);
+}
+
+
+// TODO 모달 백그라운드 닫힘 막고 / 모달 닫히는 이벤트(혹은 취소 버튼?)에 reset 달아주기 
+const addModal = document.querySelector('#add-modal');
+
+const modalInstance = new bootstrap.Modal(addModal, {
+    backdrop: 'static',
+    keyboard: false
+});
+  
+addBtnMark.addEventListener('click', function() {
+    modalInstance.show();
+});
+
+addModal.addEventListener('click', (event) => {
+    if (event.target === addModal) {
+        event.stopPropagation();
+        event.stopImmediatePropagation();
+    }
+});
+
+addModal.addEventListener('hide.bs.modal', () => {
+    console.log("닫혔다");
+})
